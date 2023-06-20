6d4448c2c5f137bd71e3b8767cd4dca205a06707
kimhs98 <kimhs982@naver.com>
2023-06-20 10:54:34 +0900

    week2 prime number v0.3

--- week2/oop2_week02.cpp
+++ week2/oop2_week02.cpp
@@ -2,20 +2,24 @@
 
 int main() {
 	int number = 0;
-	int count = 0;
+	//int count = 0;
+	bool isPrime = true;		// less momory, readability
 
 	std::cout << "Input number: ";
 	std::cin >> number;
 
 	for (int i = 2; i < number; i++)
-		if (number % i == 0)
-			count++;
+		if (number % i == 0) {
+			//count++;
+			isPrime = false;		// remove add operation
+			break;
+		}
 
-	if (count == 0)
+	//if (count == 0)
+	if (isPrime)
 		std::cout << number << " is prime number!\n";
 	else
 		std::cout << number << " is not prime number!\n";
-
 
 	return 0;
 }
