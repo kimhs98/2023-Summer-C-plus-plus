1708218afce4fc539cb6f5e2f674c9b634342c74
kimhs98 <kimhs982@naver.com>
2023-06-20 11:12:49 +0900

    week2 prime number v0.7

--- week2/oop2_week02.cpp
+++ week2/oop2_week02.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 
+bool isPrime(int n) {		// pass by value
+	if (n < 2)
+		return false;
+	else
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				//isPrime = false;
+				//break;
+				return false;
+			}
+		}
+	return true;
+}
+
 int main() {
 	int number = 0;
-	bool isPrime = true;		// less momory, readability
 
 	std::cout << "Input number: ";
 	std::cin >> number;
 
-	if (number < 2)
-		isPrime = false;
-	else
-		for (int i = 2; i < number; i++) {
-			if (number % i == 0) {
-				isPrime = false;		// remove add operation
-				break;		// loop terminates as soon as a divisor is found.
-			}
-		}
-
-	if (isPrime == true)
+	if (isPrime(number))
 		std::cout << number << " is prime number!\n";
 	else
 		std::cout << number << " is not prime number!\n";
