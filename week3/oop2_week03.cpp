8a988d3ad5d09f17d9d0f3130a7dbbe214e4633d
kimhs98 <kimhs982@naver.com>
2023-06-21 09:54:29 +0900

    week3 reference v0.2

--- week3/oop2_week03.cpp
+++ week3/oop2_week03.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 using namespace std;
 
+void test(int ra){		// call by value
+	ra = 7;
+}
+
 int main() {
 	int a = 1;
-	int& ra = a;
-	int b = 2;
-	//int& ra = b;
-	//int& rc = 99;
-	const int& rc = 99;
-	const int d = 7;
-	//int& rd = d;
-	const int& rd = d;
-
-	cout << a << " " << ra << '\n';
-	cout << rc << '\n';
-	cout << d << " " << rd << '\n';
+	//cout << &a << '\n';
+	cout << a << '\n';
+	test(a);
+	cout << a << '\n';
 
 	return 0;
 }
