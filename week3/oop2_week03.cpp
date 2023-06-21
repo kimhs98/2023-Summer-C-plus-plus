c26aa562c18e9f4e3d243f4ed1449e2ff6b05518
kimhs98 <kimhs982@naver.com>
2023-06-21 10:29:54 +0900

    week3 reference v0.4

--- week3/oop2_week03.cpp
+++ week3/oop2_week03.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void test(int &ra){		// call by reference
-	ra = 7;
-	//cout << &ra << '\n';
+void test(int *pa){		// call by pointer
+	*pa = 7;
+	cout << pa << '\n';
 }
 
 int main() {
 	int a = 1;
-	//cout << &a << '\n';
+	cout << &a << '\n';
 	cout << a << '\n';
-	test(a);
+	test(&a);
 	cout << a << '\n';
 
 	return 0;
